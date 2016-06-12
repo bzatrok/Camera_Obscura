06bf4c0a48a00a1003e380f01c555f7cc554fa73
bzatrok <bzatrok@gmail.com>
2016-06-12 10:46:00 +0200

    Initial Commit

---
+++ Camera Obscura/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera Obscura
+//
+//  Created by Bence Zátrok on 12/06/16.
+//  Copyright © 2016 Root. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
